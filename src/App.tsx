110dec8fa1273e1b70db903a76bd87cb7fd9ec41
ryokryok <mrozisan@gmail.com>
2020-02-11 13:40:11 +0900

    styled with css-in-js, version alpha

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+//import './App.css';
 import ConfigPanel from './components/ConfigPanel';
 import Text from './components/Text';
+import PanelButton from './components/PanelButton';
 import ContextProvider from './ContextProvider';
 
 function App() {
+	const [ showConfig, setShowConfig ] = useState(false);
+
 	return (
 		<ContextProvider>
-			<div className="App">
-				<ConfigPanel />
-				<Text />
-			</div>
+			<PanelButton onClick={() => setShowConfig(!showConfig)}>Show config</PanelButton>
+			{showConfig && <ConfigPanel />}
+			<Text />
 		</ContextProvider>
 	);
 }
